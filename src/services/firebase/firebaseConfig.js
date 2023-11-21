02e042b11a82720f09b659f9f0e65645a57b9f61
rogelioromo <rogelioromo25@gmail.com>
2023-11-21 14:50:19 -0800

    variables de entorno

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -3,13 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDU0jlpuvbBSa2C9JcoR2siwP1uePUugWw",
-    authDomain: "discoland-8537f.firebaseapp.com",
-    projectId: "discoland-8537f",
-    storageBucket: "discoland-8537f.appspot.com",
-    messagingSenderId: "202246820360",
-    appId: "1:202246820360:web:a4f631dcf41c1def38f7bd"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
